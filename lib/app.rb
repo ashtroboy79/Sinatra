2d85cef775df57b5c03b4b85166332c41eb64f91
ian k <kickthechair@gmail.com>
2021-08-10 16:14:58 +0100

    added post method for the cat name submission form

--- lib/app.rb
+++ lib/app.rb
@@ -22,8 +22,20 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+
+post '/named-cat' do
   p params
+  p params[:name]
   @name = params[:name]
   erb(:index)
 end
+
+
+get '/cat-form' do
+  erb(:cat_form)
+end
+
+
+
+
+
