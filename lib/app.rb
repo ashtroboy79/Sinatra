8d2dbeec8b39711791e4bf52b63e81865ddce9b0
Ahsan <rasul.ahsan@gmail.com>
2021-08-10 15:22:08 +0100

    Added conditional to index.erb

--- lib/app.rb
+++ lib/app.rb
@@ -17,6 +17,13 @@ get '/coffee' do
   "Im buzzing"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
